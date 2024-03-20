c662c7e07412037afe2495c71dace0fd85e5204e
michel-chieregato-varsomics <163292244+michel-chieregato-varsomics@users.noreply.github.com>
2024-03-20 18:44:37 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,33 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+"""
+#### Exercício 1
+
+Receba um número inteiro de um usuário. Se ele for para, imprima "Par". Se não, imprima ímpar.
+
+Exemplo:
+
+Digite um número:
+10
+
+Par
+--------
+Digite um número:
+1
+
+Ímpar
+
+Dica: Lembre do comando de resto da divisão inteira!!
+"""
+
+number = int(input("Digite um número: "))
+
+is_even = (number % 2) == 0
+
+if is_even:
+    print("Par")
+else:
+    print("Ímpar")
+  
